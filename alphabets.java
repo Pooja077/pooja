2fee07d981f0993d7145e13531fa4387006bfe05
Pooja077 <apooja1996@gmail.com>
2017-06-05 19:56:10 +0530

    Create alphabets.java

---
+++ alphabets.java
@@ -0,0 +1,10 @@
+class alphabets
+{
+public static void main(String[] args){
+char c;
+System.out.println("enter the letter"+c);
+if(c==a||e||i||o||u)
+System.out.println("vowel");
+else
+System.out.println("consonant");
+}
