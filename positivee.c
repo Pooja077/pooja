f50ac3eba1070631dd4beb275117cdb4e57d7510
Pooja077 <apooja1996@gmail.com>
2017-06-05 14:07:51 +0530

    Create positivee.c

---
+++ positivee.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the value of a:");
+scanf("%d",&a);
+{
+if(a>0)
+{if(a<0)
+printf("negative");
+else
+printf("zero")
+}
+printf("positive");
+}
