606a434937057c284a76c550f4cdf33a270c00dd
Pooja077 <apooja1996@gmail.com>
2017-06-05 20:27:34 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter a,b,c");
+scanf("%d %d %d",&a,&b,&c);
+{
+if(a>=b && a>=c)
+printf("a is big",a);
+if(b>=a && b>=c)
+printf("b is big")}
+if(c>=a && c>=b)
+printf("c is big");
+}
