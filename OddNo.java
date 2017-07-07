9000c22a1b58e4471b3ca5788d4cd0d7bd21f4f5
Pooja077 <apooja1996@gmail.com>
2017-07-07 14:32:28 +0530

    Add files via upload

---
+++ OddNo.java
@@ -0,0 +1,17 @@
+package lenn;
+import java.util.Scanner;
+public class OddNo {
+	public static void main(String args[]){
+ int x;
+ int y;
+ System.out.println("type the limit values");
+Scanner n=new Scanner(System.in);
+x=n.nextInt();
+y=n.nextInt();
+for(int i=x;i<=y;i++){
+	if(i%2!=0)
+		
+	{System.out.println(i);}
+	}
+}
+}
