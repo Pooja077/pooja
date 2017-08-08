de1fb72cc637bd7a718575384917aa83c18453df
Pooja077 <apooja1996@gmail.com>
2017-08-08 14:50:41 +0530

    Add files via upload

---
+++ RevD.java
@@ -0,0 +1,18 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class RevD {
+	public static void main(String args[]){
+		String s;
+		System.out.println("enter a number");
+		Scanner n=new Scanner(System.in);
+		s=n.nextLine();
+		for(int i=0;i<s.length();i++){
+			int z=s.length()-i;
+			System.out.print(z);
+      }
+    	  
+		//System.out.print(z);
+      }
+}
