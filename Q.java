3a317d0062235c169d0d7eeb1493d72f068e130b
Pooja077 <apooja1996@gmail.com>
2017-08-08 07:24:51 +0530

    Add files via upload

---
+++ Q.java
@@ -0,0 +1,16 @@
+package sunday;
+
+import java.util.Scanner;
+
+public class Q {
+	public static void main(String args[]){
+		System.out.println("enter the input");
+		Scanner n=new Scanner(System.in);
+		char ch=n.next().charAt(0);
+		//System.out.println("program runs until u press Q");
+		while(ch!='Q'){
+			System.out.println("program runs until u press Q");
+		}
+	}
+
+}
