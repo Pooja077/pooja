b32c6e4e497736d791baaf4c5a336f41f2104c8e
Pooja077 <apooja1996@gmail.com>
2017-08-08 15:02:55 +0530

    Add files via upload

---
+++ StoI.java
@@ -0,0 +1,14 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class StoI {
+public static void main(String args[]){
+	String s;
+	System.out.println("enter a string");
+	Scanner n=new Scanner(System.in);
+	s=n.nextLine();
+	int i=Integer.parseInt(s);
+	System.out.println(s+100);
+}
+}
